12e7c37bdf588fb8e297e20ca03e1fbad62d5cd7
Manogaran17 <manogaran2667@gmail.com>
2026-05-20 23:40:48 +0530

    Update 08-SCOREBOARD.sv

--- 01-ADDERS/01-FULL ADDER/08-SCOREBOARD.sv
+++ 01-ADDERS/01-FULL ADDER/08-SCOREBOARD.sv
@@ -1,1 +1,37 @@
+class scoreboard;
+  
+  mailbox montoscb;
+  
+  function new (mailbox montoscb);
+    
+    this.montoscb = montoscb;
+    
+  endfunction 
+  
+  task main();
+    
+    transaction trans;
+    
+    repeat(5)
+      
+      begin
+        
+        montoscb.get(trans);
+        
+        trans.display("scoreboard signals");
+        
+        // reference model
+        
+        if(((trans.a^trans.b^trans.cin)==trans.s)&&(((trans.a&trans.b)|(trans.b&trans.cin)|(trans.cin&trans.a))==trans.cout))
+          $display("====test passed===");
+          
+        else 
+          $display("===test failed===");
+        $display("---------transaction completed------------");
+        $display("                                          ");
+        
+      end 
+  endtask
+endclass
 
+          
