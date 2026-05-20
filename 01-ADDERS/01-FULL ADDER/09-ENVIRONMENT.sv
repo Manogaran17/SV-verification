56cfee117723f2bc3c32d0892e9b89eb4c9337a4
Manogaran17 <manogaran2667@gmail.com>
2026-05-20 23:41:08 +0530

    Update 09-ENVIRONMENT.sv

--- 01-ADDERS/01-FULL ADDER/09-ENVIRONMENT.sv
+++ 01-ADDERS/01-FULL ADDER/09-ENVIRONMENT.sv
@@ -1,1 +1,46 @@
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
 
+class environment;
+  
+  generator gen;
+  driver drv;
+  monitor mon;
+  scoreboard scb;
+  event done;
+  
+  mailbox gentodrv;
+  mailbox montoscb;
+  
+  virtual inter vif;
+  
+  function new (virtual inter vif);
+    
+    this.vif = vif;
+    
+    gentodrv = new();
+    montoscb = new();
+    gen = new(gentodrv);
+    drv = new (vif,gentodrv,done);
+    mon = new(vif,montoscb,done);
+    scb = new(montoscb);
+    
+  endfunction
+  
+  task test_run();
+    
+    fork
+      gen.main();
+      drv.main();
+      mon.main();
+      scb.main();
+    join
+    
+  endtask
+  
+endclass
+  
+  
