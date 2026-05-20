4bd50785c5cb823fe169c401bc369b932fa575d4
Manogaran17 <manogaran2667@gmail.com>
2026-05-20 23:40:08 +0530

    Update 06-DRIVER.sv

--- 01-ADDERS/01-FULL ADDER/06-DRIVER.sv
+++ 01-ADDERS/01-FULL ADDER/06-DRIVER.sv
@@ -1,1 +1,40 @@
+class driver;
+  
+  virtual inter vif;
+  event done;
+  
+  mailbox gentodrv;
+  
+  function new (virtual inter vif,mailbox gentodrv,event done);
+    
+    this.vif = vif;
+    this.gentodrv = gentodrv;
+    this.done=done;
+    endfunction
+  
+  task main();
+    
+    repeat(5)
+      begin
+        
+        transaction trans;
+        
+        gentodrv.get(trans);
+        
+        // driver to get the transaction from  mailbox to driver
+        
+         vif.a <= trans.a;
+         vif.b <= trans.b;
+         vif.cin <= trans.cin;
+        #1;
+        ->done;
+        trans.display("driver class signals");
+        
+        
+      end 
+    
+  endtask
+  
+endclass
 
+        
