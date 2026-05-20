ee8536a99ebf511af7830acc000c8ceabc98bec4
Manogaran17 <manogaran2667@gmail.com>
2026-05-20 23:40:26 +0530

    Update 07-MONITOR.sv

--- 01-ADDERS/01-FULL ADDER/07-MONITOR.sv
+++ 01-ADDERS/01-FULL ADDER/07-MONITOR.sv
@@ -1,1 +1,43 @@
+class monitor;
+  
+  virtual inter vif;
+  event done;
+  
+  mailbox montoscb;
+  
+  function new (virtual inter vif, mailbox montoscb,event done);
+    
+    this.vif = vif;
+    this.montoscb = montoscb;
+    this.done = done;
+    
+    
+  endfunction
+  
+  task main();
+    
+    repeat(5)
+     
+      begin
+        
+        transaction trans;
+        
+        @(done);
+         trans = new();
+         trans.a = vif.a;
+         trans.b = vif.b;
+         trans.cin = vif.cin;
+         trans.s = vif.s;
+         trans.cout = vif.cout;
+        
+        montoscb.put(trans);
+        
+        trans.display("monitor class signals");
+        
+      end 
+  endtask
+  
+endclass
 
+
+        
