b38b181e34bc997942366a9c09f35540e8339c07
Manogaran17 <manogaran2667@gmail.com>
2026-05-20 23:39:29 +0530

    Update 05-GENERATOR.sv

--- 01-ADDERS/01-FULL ADDER/05-GENERATOR.sv
+++ 01-ADDERS/01-FULL ADDER/05-GENERATOR.sv
@@ -1,1 +1,30 @@
-
+class generator;
+  
+  transaction trans;
+  
+  mailbox gentodrv;
+  
+  function new ( mailbox gentodrv);
+    
+    this.gentodrv=gentodrv;
+    
+  endfunction
+  
+  task main();
+    
+    repeat (5)
+      begin
+        
+        trans = new();
+        
+        trans.randomize();
+        
+        trans.display("generator class signals");
+        
+        gentodrv.put(trans);
+        
+        #10;
+        
+      end 
+  endtask
+endclass
