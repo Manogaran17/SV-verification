cbb471b8e46861916b450af8ae0e4b64bfaba77c
Manogaran17 <manogaran2667@gmail.com>
2026-05-25 20:13:26 +0530

    Update 03-INTERFACE.sv

--- 03-SHIFT REGISTER/01-PIPO/03-INTERFACE.sv
+++ 03-SHIFT REGISTER/01-PIPO/03-INTERFACE.sv
@@ -1,1 +1,19 @@
 
+
+interface pipo_if(input logic clk);
+  
+    logic rst;
+    logic [3:0] parallel_in;
+    logic [3:0] parallel_out;
+
+
+    clocking cb @(posedge clk);
+ 
+        output rst;
+        output parallel_in;
+
+        input parallel_out;
+
+    endclocking
+
+endinterface
