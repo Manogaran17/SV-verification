661a3e7c9579dc23b01b06e52f9ffdb9cad00af1
Manogaran17 <manogaran2667@gmail.com>
2026-05-25 20:13:01 +0530

    Update 02-TESTBENCH_TOP.sv

--- 03-SHIFT REGISTER/01-PIPO/02-TESTBENCH_TOP.sv
+++ 03-SHIFT REGISTER/01-PIPO/02-TESTBENCH_TOP.sv
@@ -1,1 +1,37 @@
+`include "interface.sv"
+`include "test.sv"
 
+
+module top_tb;
+
+    logic clk;
+
+
+    initial
+      begin
+
+        clk = 0;
+        forever #10 clk = ~clk;
+    end
+
+    pipo_if vif(clk); 
+
+    pipo_shift_register dut 
+    (
+        .clk(clk),
+        .rst(vif.rst),
+        .parallel_in(vif.parallel_in),
+        .parallel_out(vif.parallel_out)
+    );
+
+    test t1(vif);
+
+    initial
+    begin
+
+        $dumpfile("dump.vcd");
+        $dumpvars;
+        #250 $finish;
+    end
+
+endmodule
