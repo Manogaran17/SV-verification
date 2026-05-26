3e2005a92b2fc2412c3a6c37ff36f1ffaf0d3354
Manogaran17 <manogaran2667@gmail.com>
2026-05-26 20:06:03 +0530

    Update 05-GENERATOR.sv

--- 03-SHIFT REGISTER/01-PIPO/05-GENERATOR.sv
+++ 03-SHIFT REGISTER/01-PIPO/05-GENERATOR.sv
@@ -1,1 +1,37 @@
+class generator;
 
+    transaction trans;
+    mailbox gentodrv;
+
+    virtual pipo_if vif;
+    function new(mailbox gentodrv, virtual pipo_if vif);
+
+        this.gentodrv=gentodrv;
+        this.vif=vif;
+
+    endfunction
+
+
+    task main();
+
+        repeat(10)
+           begin
+             
+             @(vif.drv_cb);
+
+            trans=new();
+
+            trans.randomize();
+             
+             $display("[GENERATOR ] Time=%0t | clk=%0b | rst=%0b | pin=%0b | pout=%0b",
+                      $time,
+                      vif.clk,
+                      trans.rst,
+                      trans.parallel_in,
+                      trans.parallel_out);
+
+            gentodrv.put(trans);
+          
+        end
+    endtask
+endclass
