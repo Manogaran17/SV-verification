c73fd9275256ac3140deba8375de470638c1b411
Manogaran17 <manogaran2667@gmail.com>
2026-05-26 20:06:25 +0530

    Update 06-DRIVER.sv

--- 03-SHIFT REGISTER/01-PIPO/06-DRIVER.sv
+++ 03-SHIFT REGISTER/01-PIPO/06-DRIVER.sv
@@ -1,1 +1,41 @@
+class driver;
+
+    virtual pipo_if vif;
+    mailbox gentodrv;
+
+    function new(virtual pipo_if vif, mailbox gentodrv);
+
+        this.vif=vif;
+        this.gentodrv=gentodrv;
+
+    endfunction
+
+
+    task main();
+        transaction trans;
+
+        repeat(10)
+             begin
+               
+               @(vif.drv_cb);
+
+            gentodrv.get(trans);
+
+            vif.drv_cb.parallel_in<=trans.parallel_in;
+
+            vif.drv_cb.rst<=trans.rst;
+               
+                $display("[DRIVER    ] Time=%0t | clk=%0b | rst=%0b | pin=%0b | pout=%0b",
+                      $time,
+                      vif.clk,
+                      trans.rst,
+                      trans.parallel_in,
+                      trans.parallel_out);
+
+        
+        end
+
+    endtask
+
+endclass
 
