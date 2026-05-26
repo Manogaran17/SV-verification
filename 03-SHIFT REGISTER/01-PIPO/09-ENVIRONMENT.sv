4298b5fb7c74cb304a151682c490c0de9e9fe15c
Manogaran17 <manogaran2667@gmail.com>
2026-05-26 20:07:34 +0530

    Update 09-ENVIRONMENT.sv

--- 03-SHIFT REGISTER/01-PIPO/09-ENVIRONMENT.sv
+++ 03-SHIFT REGISTER/01-PIPO/09-ENVIRONMENT.sv
@@ -1,1 +1,43 @@
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
 
+class environment;
+
+    generator gen;
+    driver drv;
+    monitor mon;
+    scoreboard scb;
+  
+    mailbox gentodrv;
+    mailbox montoscb;
+
+    virtual pipo_if vif;
+
+    function new(virtual pipo_if vif);
+
+        this.vif = vif;
+
+        gentodrv = new();
+        montoscb = new();
+        gen = new(gentodrv,vif);
+        drv = new(vif,gentodrv);
+        mon = new(vif,montoscb);
+        scb = new(montoscb);
+
+    endfunction
+
+    task run();
+
+        fork
+            gen.main();
+            drv.main();
+            mon.main();
+            scb.main();
+        join_any
+
+    endtask
+
+endclass
