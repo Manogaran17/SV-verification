42d8d582aaf59c5307377b26140cf0155a96b8ff
Manogaran17 <manogaran2667@gmail.com>
2026-05-26 20:09:02 +0530

    Create 11-OUTPUT.sv

---
+++ 03-SHIFT REGISTER/01-PIPO/11-OUTPUT.sv
@@ -0,0 +1,53 @@
+Compiler version X-2025.06-SP1_Full64; Runtime version X-2025.06-SP1_Full64;  May 26 10:32 2026
+[GENERATOR ] Time=10 | clk=1 | rst=1 | pin=1000 | pout=0
+[DRIVER    ] Time=10 | clk=1 | rst=1 | pin=1000 | pout=0
+[MONITOR   ] Time=10 | clk=1 | rst=x | pin=xxxx | pout=xxxx
+[SCOREBOARD] Time=10 | clk=1 | rst=0 | pin=0 | pout=0 | TEST PASSED
+
+[GENERATOR ] Time=30 | clk=1 | rst=0 | pin=1111 | pout=0
+[DRIVER    ] Time=30 | clk=1 | rst=0 | pin=1111 | pout=0
+[MONITOR   ] Time=30 | clk=1 | rst=1 | pin=1000 | pout=0
+[SCOREBOARD] Time=30 | clk=1 | rst=1 | pin=1000 | pout=0 | TEST PASSED
+
+[GENERATOR ] Time=50 | clk=1 | rst=0 | pin=1000 | pout=0
+[DRIVER    ] Time=50 | clk=1 | rst=0 | pin=1000 | pout=0
+[MONITOR   ] Time=50 | clk=1 | rst=0 | pin=1111 | pout=1111
+[SCOREBOARD] Time=50 | clk=1 | rst=0 | pin=1111 | pout=1111 | TEST PASSED
+
+[GENERATOR ] Time=70 | clk=1 | rst=0 | pin=1 | pout=0
+[DRIVER    ] Time=70 | clk=1 | rst=0 | pin=1 | pout=0
+[MONITOR   ] Time=70 | clk=1 | rst=0 | pin=1000 | pout=1000
+[SCOREBOARD] Time=70 | clk=1 | rst=0 | pin=1000 | pout=1000 | TEST PASSED
+
+[GENERATOR ] Time=90 | clk=1 | rst=1 | pin=1100 | pout=0
+[DRIVER    ] Time=90 | clk=1 | rst=1 | pin=1100 | pout=0
+[MONITOR   ] Time=90 | clk=1 | rst=0 | pin=1 | pout=1
+[SCOREBOARD] Time=90 | clk=1 | rst=0 | pin=1 | pout=1 | TEST PASSED
+
+[GENERATOR ] Time=110 | clk=1 | rst=0 | pin=10 | pout=0
+[DRIVER    ] Time=110 | clk=1 | rst=0 | pin=10 | pout=0
+[MONITOR   ] Time=110 | clk=1 | rst=1 | pin=1100 | pout=0
+[SCOREBOARD] Time=110 | clk=1 | rst=1 | pin=1100 | pout=0 | TEST PASSED
+
+[GENERATOR ] Time=130 | clk=1 | rst=1 | pin=111 | pout=0
+[DRIVER    ] Time=130 | clk=1 | rst=1 | pin=111 | pout=0
+[MONITOR   ] Time=130 | clk=1 | rst=0 | pin=10 | pout=10
+[SCOREBOARD] Time=130 | clk=1 | rst=0 | pin=10 | pout=10 | TEST PASSED
+
+[GENERATOR ] Time=150 | clk=1 | rst=1 | pin=1001 | pout=0
+[DRIVER    ] Time=150 | clk=1 | rst=1 | pin=1001 | pout=0
+[MONITOR   ] Time=150 | clk=1 | rst=1 | pin=111 | pout=0
+[SCOREBOARD] Time=150 | clk=1 | rst=1 | pin=111 | pout=0 | TEST PASSED
+
+[GENERATOR ] Time=170 | clk=1 | rst=0 | pin=10 | pout=0
+[DRIVER    ] Time=170 | clk=1 | rst=0 | pin=10 | pout=0
+[MONITOR   ] Time=170 | clk=1 | rst=1 | pin=1001 | pout=0
+[SCOREBOARD] Time=170 | clk=1 | rst=1 | pin=1001 | pout=0 | TEST PASSED
+
+[GENERATOR ] Time=190 | clk=1 | rst=1 | pin=1 | pout=0
+[DRIVER    ] Time=190 | clk=1 | rst=1 | pin=1 | pout=0
+[MONITOR   ] Time=190 | clk=1 | rst=0 | pin=10 | pout=10
+[SCOREBOARD] Time=190 | clk=1 | rst=0 | pin=10 | pout=10 | TEST PASSED
+
+$finish at simulation time                  190
+           V C S   S i m u l a t i o n   R e p o r t 
