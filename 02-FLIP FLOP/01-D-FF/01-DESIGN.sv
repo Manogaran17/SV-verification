d394b56cbace839ac6c4ac0736c713494a6fb498
Manogaran17 <manogaran2667@gmail.com>
2026-05-27 20:30:45 +0530

    Update 01-DESIGN.sv

--- 02-FLIP FLOP/01-D-FF/01-DESIGN.sv
+++ 02-FLIP FLOP/01-D-FF/01-DESIGN.sv
@@ -1,1 +1,20 @@
+// Code your design here
+
+module d_ff(
+
+  input d,
+  input clk,
+  input rst,
+  output reg q
+);
+  
+  always@(posedge clk)
+    
+    begin
+      if(rst)
+        q<=0;
+      else 
+        q<=d;
+    end 
+endmodule 
 
