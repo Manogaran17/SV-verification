8db730ce160f7f7f39fa6a9071c9dcb11b2749f6
Manogaran17 <manogaran2667@gmail.com>
2026-05-27 20:34:16 +0530

    Update 09-ENVIRONMENT.sv

--- 02-FLIP FLOP/01-D-FF/09-ENVIRONMENT.sv
+++ 02-FLIP FLOP/01-D-FF/09-ENVIRONMENT.sv
@@ -1,1 +1,53 @@
+`include "transaction.sv"
+`include "generator.sv" 
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
 
+
+class environment;
+  
+  virtual inter vif;
+  
+  generator gen;
+  
+  driver drv;
+  
+  monitor mon;
+  
+  scoreboard scb;
+  
+  mailbox gentodrv;
+  mailbox montoscb;
+  
+  function new(virtual inter vif);
+    
+    this.vif = vif;
+    
+    montoscb = new();
+    gentodrv = new();
+    
+    gen = new(gentodrv,vif);
+    drv = new(vif,gentodrv);
+    mon = new(vif,montoscb);
+    scb = new(montoscb,vif);
+    
+  endfunction 
+  
+  task run();
+    
+    fork
+      
+      gen.main();
+      drv.main();
+      mon.main();
+      scb.main();
+      
+    join
+    
+  endtask
+   
+    endclass
+    
+  
+  
