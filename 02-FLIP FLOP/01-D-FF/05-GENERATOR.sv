e55f01e70c41cf87c6a9f1d1ffb23774c1ac247c
Manogaran17 <manogaran2667@gmail.com>
2026-05-27 20:32:25 +0530

    Update 05-GENERATOR.sv

--- 02-FLIP FLOP/01-D-FF/05-GENERATOR.sv
+++ 02-FLIP FLOP/01-D-FF/05-GENERATOR.sv
@@ -1,1 +1,38 @@
+class generator;
 
+    transaction trans;
+    mailbox gentodrv;
+
+    virtual inter vif;
+  
+  function new(mailbox gentodrv, virtual inter vif);
+
+        this.gentodrv=gentodrv;
+        this.vif=vif;
+
+    endfunction
+
+
+    task main();
+
+        repeat(10)
+           begin
+             
+             @(vif.cb);
+
+            trans=new();
+
+            trans.randomize();
+             
+             $display("[GENERATOR  ] Time=%0t | clk=%0b | rst=%0b | d=%0b | q=%0b",
+                      $time,
+                      vif.clk,
+                      trans.rst,
+                      trans.d,
+                      trans.q);
+
+            gentodrv.put(trans);
+          
+        end
+    endtask
+endclass
