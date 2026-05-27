2ae76a67b3a17a9cf7200d8a531aa817c80754b3
Manogaran17 <manogaran2667@gmail.com>
2026-05-27 20:35:23 +0530

    Update 11-OUTPUT.sv

--- 02-FLIP FLOP/01-D-FF/11-OUTPUT.sv
+++ 02-FLIP FLOP/01-D-FF/11-OUTPUT.sv
@@ -1,1 +1,52 @@
-
+GENERATOR  ] Time=5 | clk=1 | rst=1 | d=0 | q=0
+[DRIVER    ] Time=5 | clk=1 | rst=1 | d=0 | q=0
+[MONITOR   ] Time=5 | clk=1 | rst=x | d=x | q=x
+[SCOREBOARD] Time=5 | clk=1 | rst=0 | d=0 | q=0 | D_FF TEST PASSED 
+______________________________________________________________
+[GENERATOR  ] Time=15 | clk=1 | rst=0 | d=1 | q=0
+[DRIVER    ] Time=15 | clk=1 | rst=0 | d=1 | q=0
+[MONITOR   ] Time=15 | clk=1 | rst=1 | d=0 | q=0
+[SCOREBOARD] Time=15 | clk=1 | rst=1 | d=0 | q=0 | RESET TEST PASSED 
+______________________________________________________________
+[GENERATOR  ] Time=25 | clk=1 | rst=0 | d=0 | q=0
+[DRIVER    ] Time=25 | clk=1 | rst=0 | d=0 | q=0
+[MONITOR   ] Time=25 | clk=1 | rst=0 | d=1 | q=1
+[SCOREBOARD] Time=25 | clk=1 | rst=0 | d=1 | q=1 | D_FF TEST PASSED 
+______________________________________________________________
+[GENERATOR  ] Time=35 | clk=1 | rst=0 | d=1 | q=0
+[DRIVER    ] Time=35 | clk=1 | rst=0 | d=1 | q=0
+[MONITOR   ] Time=35 | clk=1 | rst=0 | d=0 | q=0
+[SCOREBOARD] Time=35 | clk=1 | rst=0 | d=0 | q=0 | D_FF TEST PASSED 
+______________________________________________________________
+[GENERATOR  ] Time=45 | clk=1 | rst=1 | d=0 | q=0
+[DRIVER    ] Time=45 | clk=1 | rst=1 | d=0 | q=0
+[MONITOR   ] Time=45 | clk=1 | rst=0 | d=1 | q=1
+[SCOREBOARD] Time=45 | clk=1 | rst=0 | d=1 | q=1 | D_FF TEST PASSED 
+______________________________________________________________
+[GENERATOR  ] Time=55 | clk=1 | rst=0 | d=0 | q=0
+[DRIVER    ] Time=55 | clk=1 | rst=0 | d=0 | q=0
+[MONITOR   ] Time=55 | clk=1 | rst=1 | d=0 | q=0
+[SCOREBOARD] Time=55 | clk=1 | rst=1 | d=0 | q=0 | RESET TEST PASSED 
+______________________________________________________________
+[GENERATOR  ] Time=65 | clk=1 | rst=1 | d=1 | q=0
+[DRIVER    ] Time=65 | clk=1 | rst=1 | d=1 | q=0
+[MONITOR   ] Time=65 | clk=1 | rst=0 | d=0 | q=0
+[SCOREBOARD] Time=65 | clk=1 | rst=0 | d=0 | q=0 | D_FF TEST PASSED 
+______________________________________________________________
+[GENERATOR  ] Time=75 | clk=1 | rst=1 | d=1 | q=0
+[DRIVER    ] Time=75 | clk=1 | rst=1 | d=1 | q=0
+[MONITOR   ] Time=75 | clk=1 | rst=1 | d=1 | q=0
+[SCOREBOARD] Time=75 | clk=1 | rst=1 | d=1 | q=0 | RESET TEST PASSED 
+______________________________________________________________
+[GENERATOR  ] Time=85 | clk=1 | rst=0 | d=0 | q=0
+[DRIVER    ] Time=85 | clk=1 | rst=0 | d=0 | q=0
+[MONITOR   ] Time=85 | clk=1 | rst=1 | d=1 | q=0
+[SCOREBOARD] Time=85 | clk=1 | rst=1 | d=1 | q=0 | RESET TEST PASSED 
+______________________________________________________________
+[GENERATOR  ] Time=95 | clk=1 | rst=1 | d=1 | q=0
+[DRIVER    ] Time=95 | clk=1 | rst=1 | d=1 | q=0
+[MONITOR   ] Time=95 | clk=1 | rst=0 | d=0 | q=0
+[SCOREBOARD] Time=95 | clk=1 | rst=0 | d=0 | q=0 | D_FF TEST PASSED 
+______________________________________________________________
+$finish at simulation time                   95
+           V C S   S i m u l a t i o n   R e p o r t 
