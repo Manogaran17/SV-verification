1e779c523270d0e1d6df0af165ed2399d43fbfa4
Manogaran17 <manogaran2667@gmail.com>
2026-05-27 20:31:49 +0530

    Update 03-INTERFACE.sv

--- 02-FLIP FLOP/01-D-FF/03-INTERFACE.sv
+++ 02-FLIP FLOP/01-D-FF/03-INTERFACE.sv
@@ -1,1 +1,20 @@
 
+interface inter(input logic clk);
+  
+    logic d;
+    logic rst;
+   
+    logic q;
+
+
+    clocking cb @(posedge clk);
+      
+     default input #1step output #0;
+ 
+        output rst;
+        output d;
+        input  q;
+
+    endclocking
+
+endinterface
