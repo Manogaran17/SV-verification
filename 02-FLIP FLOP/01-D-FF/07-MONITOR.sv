5d5b624521c53a2ef256f094711b86a939a39dea
Manogaran17 <manogaran2667@gmail.com>
2026-05-27 20:33:31 +0530

    Update 07-MONITOR.sv

--- 02-FLIP FLOP/01-D-FF/07-MONITOR.sv
+++ 02-FLIP FLOP/01-D-FF/07-MONITOR.sv
@@ -1,1 +1,42 @@
+class monitor;
+  
+  transaction trans;
+  
+  mailbox montoscb;
+  
+  virtual inter vif;
+  
+  function new(virtual inter vif,mailbox montoscb);
+    
+    this.vif=vif;
+    
+    this.montoscb=montoscb;
+    
+  endfunction
+  
+  task main();
+    
+    repeat(10)
+      begin
+        
+        @(vif.cb);
+        
+        trans=new();
+        
+        trans.d =vif.d;
+        trans.rst =vif.rst;
+        trans.q =vif.q;
+        
+        $display("[MONITOR   ] Time=%0t | clk=%0b | rst=%0b | d=%0b | q=%0b",
+                      $time,
+                      vif.clk,
+                      vif.rst,
+                      vif.d,
+                      vif.q);
+
+        
+        montoscb.put(trans);
+      end 
+  endtask
+endclass
 
