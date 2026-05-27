9b3efa37fc448c236af70d788f521afdff3f30cc
Manogaran17 <manogaran2667@gmail.com>
2026-05-27 20:31:06 +0530

    Update 02-TESTBENCH_TOP.sv

--- 02-FLIP FLOP/01-D-FF/02-TESTBENCH_TOP.sv
+++ 02-FLIP FLOP/01-D-FF/02-TESTBENCH_TOP.sv
@@ -1,1 +1,42 @@
+// Code your testbench here
+// or browse Examples
 
+`include "interface.sv"
+`include "test.sv"
+
+module top_tb;
+  
+  logic clk;
+  
+  initial 
+    begin
+      
+      clk=0;
+      forever #5 clk = ~clk;
+      
+    end 
+  
+  inter vif(clk);
+  
+  d_ff dut(
+  
+    .d(vif.d),
+    .clk(vif.clk),
+    .rst(vif.rst),
+    .q(vif.q)
+  );
+  
+  test t1(vif);
+  
+    initial
+    begin
+
+        $dumpfile("dump.vcd");
+        $dumpvars;
+        #250 $finish;
+    end
+
+endmodule
+  
+  
+  
