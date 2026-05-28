9b61cb0ccd18c846cce5ea65a04c694f5fb198de
Manogaran17 <manogaran2667@gmail.com>
2026-05-28 19:51:39 +0530

    Update 01-DESIGN.sv

--- 04-MEMORY/01-SRAM/01-DESIGN.sv
+++ 04-MEMORY/01-SRAM/01-DESIGN.sv
@@ -1,1 +1,27 @@
+// Code your design here
+// Code your design here
+
+module single_portRAM(clk,we,rst,data_in,data_out,addr);
+  
+  input clk,rst,we;
+  input [2:0]addr;
+  input [7:0]data_in;
+  output reg [7:0]data_out;
+  reg [7:0]mem[7:0];
+  
+  always@(posedge clk or posedge rst)
+    begin 
+      
+      if(rst)
+        begin 
+        data_out<=0;
+        mem[addr]<=0;
+        end 
+      else if(we)
+        mem[addr]<=data_in;
+      else 
+        data_out<=mem[addr];
+    end 
+  
+endmodule 
 
