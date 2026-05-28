029437a3b9147280252718cec9644a97b50aeab4
Manogaran17 <manogaran2667@gmail.com>
2026-05-28 20:06:02 +0530

    Update 06-DRIVER.sv

--- 04-MEMORY/01-SRAM/06-DRIVER.sv
+++ 04-MEMORY/01-SRAM/06-DRIVER.sv
@@ -1,1 +1,49 @@
+class driver;
+  
+  transaction trans;
+  
+  mailbox #(transaction) gentodrv;
+  
+  virtual inter vif;
+  
+  
+  function new(virtual inter vif, mailbox #(transaction) gentodrv);
+    
+    this.vif =  vif;
+    
+    this.gentodrv = gentodrv;
+    
+    endfunction
+  
+  task main();
+    
+    repeat(10)
+      begin
+        
+           @(vif.cb);
+        
+        gentodrv.get(trans);
+        
+        vif.cb.rst <= trans.rst;
+        vif.cb.we <= trans.we;
+        vif.cb.addr <= trans.addr;
+        
+        vif.cb.data_in <= trans.data_in;
+        
+        $display("[DRIVER     ] |time = %0t |clk = %0b|rst = %b |we = %b|addr = %d|data_in = %d|data_out = %d",
+                $time,
+                 vif.clk,
+                 trans.rst,
+                 trans.we,
+                 trans.addr,
+                 trans.data_in,
+                 trans.data_out
+                );
+        
+      end 
+  endtask
+endclass:driver 
 
+
+
+  
