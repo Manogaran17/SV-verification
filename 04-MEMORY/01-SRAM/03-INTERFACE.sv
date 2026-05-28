8100724331cd8f1ea932ee1c146e01d326ffee9b
Manogaran17 <manogaran2667@gmail.com>
2026-05-28 20:01:59 +0530

    Update 03-INTERFACE.sv

--- 04-MEMORY/01-SRAM/03-INTERFACE.sv
+++ 04-MEMORY/01-SRAM/03-INTERFACE.sv
@@ -1,1 +1,22 @@
-
+interface inter ( input logic clk);
+  
+  logic rst;
+  logic we;
+  logic [2:0]addr;
+  logic [7:0]data_in;
+  logic [7:0]data_out;
+  
+  clocking cb @(posedge clk );
+    
+    default input #1step output #0;
+    
+    output  rst;
+    output  we;
+    output  addr;
+    output data_in;
+    
+    input  data_out;
+    
+  endclocking 
+  
+endinterface 
