3ee1c385e72287345478a697fdb79876f77a3543
Manogaran17 <manogaran2667@gmail.com>
2026-05-28 20:08:29 +0530

    Update 11-OUTPUT.sv

--- 04-MEMORY/01-SRAM/11-OUTPUT.sv
+++ 04-MEMORY/01-SRAM/11-OUTPUT.sv
@@ -1,1 +1,52 @@
-
+[GENERATOR  ] |time = 10 |clk = 1|rst = 0 |we = 1|addr = 0|data_in = 133|data_out =   0
+[DRIVER     ] |time = 10 |clk = 1|rst = 0 |we = 1|addr = 0|data_in = 133|data_out =   0
+[MONITOR    ] |time = 10 |clk = 1|rst = x |we = x|addr = x|data_in =   x|data_out =   x
+[SCOREBOARD ] |time = 10|clk = 1|rst = 0|we  = 0|addr = 0|data_in = 0|data_out = 0|READ TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 30 |clk = 1|rst = 0 |we = 1|addr = 7|data_in = 158|data_out =   0
+[DRIVER     ] |time = 30 |clk = 1|rst = 0 |we = 1|addr = 7|data_in = 158|data_out =   0
+[MONITOR    ] |time = 30 |clk = 1|rst = 0 |we = 1|addr = 0|data_in = 133|data_out =   x
+[SCOREBOARD ] |time = 30|clk = 1|rst = 0|we = 1|addr = 0|data_in = 133|data_out = 0|WRITE TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 50 |clk = 1|rst = 0 |we = 1|addr = 0|data_in = 186|data_out =   0
+[DRIVER     ] |time = 50 |clk = 1|rst = 0 |we = 1|addr = 0|data_in = 186|data_out =   0
+[MONITOR    ] |time = 50 |clk = 1|rst = 0 |we = 1|addr = 7|data_in = 158|data_out =   x
+[SCOREBOARD ] |time = 50|clk = 1|rst = 0|we = 1|addr = 7|data_in = 158|data_out = 0|WRITE TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 70 |clk = 1|rst = 0 |we = 1|addr = 1|data_in =  63|data_out =   0
+[DRIVER     ] |time = 70 |clk = 1|rst = 0 |we = 1|addr = 1|data_in =  63|data_out =   0
+[MONITOR    ] |time = 70 |clk = 1|rst = 0 |we = 1|addr = 0|data_in = 186|data_out =   x
+[SCOREBOARD ] |time = 70|clk = 1|rst = 0|we = 1|addr = 0|data_in = 186|data_out = 0|WRITE TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 90 |clk = 1|rst = 0 |we = 1|addr = 4|data_in =  68|data_out =   0
+[DRIVER     ] |time = 90 |clk = 1|rst = 0 |we = 1|addr = 4|data_in =  68|data_out =   0
+[MONITOR    ] |time = 90 |clk = 1|rst = 0 |we = 1|addr = 1|data_in =  63|data_out =   x
+[SCOREBOARD ] |time = 90|clk = 1|rst = 0|we = 1|addr = 1|data_in = 63|data_out = 0|WRITE TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 110 |clk = 1|rst = 0 |we = 0|addr = 4|data_in =   0|data_out =   0
+[DRIVER     ] |time = 110 |clk = 1|rst = 0 |we = 0|addr = 4|data_in =   0|data_out =   0
+[MONITOR    ] |time = 110 |clk = 1|rst = 0 |we = 1|addr = 4|data_in =  68|data_out =   x
+[SCOREBOARD ] |time = 110|clk = 1|rst = 0|we = 1|addr = 4|data_in = 68|data_out = 0|WRITE TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 130 |clk = 1|rst = 0 |we = 0|addr = 1|data_in =   0|data_out =   0
+[DRIVER     ] |time = 130 |clk = 1|rst = 0 |we = 0|addr = 1|data_in =   0|data_out =   0
+[MONITOR    ] |time = 130 |clk = 1|rst = 0 |we = 0|addr = 4|data_in =   0|data_out =  68
+[SCOREBOARD ] |time = 130|clk = 1|rst = 0|we  = 0|addr = 4|data_in = 0|data_out = 68|READ TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 150 |clk = 1|rst = 0 |we = 0|addr = 0|data_in =   0|data_out =   0
+[DRIVER     ] |time = 150 |clk = 1|rst = 0 |we = 0|addr = 0|data_in =   0|data_out =   0
+[MONITOR    ] |time = 150 |clk = 1|rst = 0 |we = 0|addr = 1|data_in =   0|data_out =  63
+[SCOREBOARD ] |time = 150|clk = 1|rst = 0|we  = 0|addr = 1|data_in = 0|data_out = 63|READ TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 170 |clk = 1|rst = 0 |we = 0|addr = 7|data_in =   0|data_out =   0
+[DRIVER     ] |time = 170 |clk = 1|rst = 0 |we = 0|addr = 7|data_in =   0|data_out =   0
+[MONITOR    ] |time = 170 |clk = 1|rst = 0 |we = 0|addr = 0|data_in =   0|data_out = 186
+[SCOREBOARD ] |time = 170|clk = 1|rst = 0|we  = 0|addr = 0|data_in = 0|data_out = 186|READ TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 190 |clk = 1|rst = 0 |we = 0|addr = 7|data_in =   0|data_out =   0
+[DRIVER     ] |time = 190 |clk = 1|rst = 0 |we = 0|addr = 7|data_in =   0|data_out =   0
+[MONITOR    ] |time = 190 |clk = 1|rst = 0 |we = 0|addr = 7|data_in =   0|data_out = 158
+[SCOREBOARD ] |time = 190|clk = 1|rst = 0|we  = 0|addr = 7|data_in = 0|data_out = 158|READ TEST PASSED
+________________________________________________________________
+$finish at simulation time                  190
+           V C S   S i m u l a t i o n   R e p o r t 
