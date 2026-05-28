12ce9428e58360622507c5b3cc7e43276f8a8a3f
Manogaran17 <manogaran2667@gmail.com>
2026-05-28 20:03:48 +0530

    Update 04-TRANSACTION.sv

--- 04-MEMORY/01-SRAM/04-TRANSACTION.sv
+++ 04-MEMORY/01-SRAM/04-TRANSACTION.sv
@@ -1,1 +1,29 @@
-
+class transaction;
+  
+   bit rst;
+  rand bit we;
+  rand bit [2:0]addr;
+  rand bit [7:0]data_in;
+  
+  bit [7:0]data_out;
+  static int count;
+  
+  constraint wr_c
+  {
+    if(count < 5)
+      we == 1;
+    else 
+      we == 0;
+  }
+  
+  constraint rd_c
+  {
+  if(we==0)
+    data_in==0;
+  }
+  
+  function void post_randomize(); 
+    count++;
+  endfunction 
+  
+endclass 
