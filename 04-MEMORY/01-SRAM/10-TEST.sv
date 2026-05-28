a8a64c07ae30cea97d43a97fa77c09642ce75d40
Manogaran17 <manogaran2667@gmail.com>
2026-05-28 20:07:47 +0530

    Update 10-TEST.sv

--- 04-MEMORY/01-SRAM/10-TEST.sv
+++ 04-MEMORY/01-SRAM/10-TEST.sv
@@ -1,1 +1,14 @@
-
+`include "environment.sv"
+program test(inter vif);
+  
+  environment env;
+  
+  initial 
+    begin
+      
+      env = new(vif);
+      env.run();
+      
+    end
+  
+endprogram 
