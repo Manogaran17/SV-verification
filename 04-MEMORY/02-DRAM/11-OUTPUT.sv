c2a21f742e74408be720074cf4532adb8cd5e7e4
Manogaran17 <manogaran2667@gmail.com>
2026-05-29 13:14:33 +0530

    Update 11-OUTPUT.sv

--- 04-MEMORY/02-DRAM/11-OUTPUT.sv
+++ 04-MEMORY/02-DRAM/11-OUTPUT.sv
@@ -1,1 +1,74 @@
-
+[GENERATOR  ] |time = 10 |clk = 1|rst = 0 |we1 = 1|we2 = 1|addr1 = 1|addr2 = 3|data_in1 =   7|data_in2 = 169|data_out1 =   0|data_out2 =   0
+[DRIVER  ] |time = 10 |clk = 1|rst = 0 |we1 = 1|we2 = 1|addr1 = 1|addr2 = 3|data_in1 =   7|data_in2 = 169|data_out1 =   0|data_out2 =   0
+[MONITOR  ] |time = 10 |clk = 1|rst = x |we1 = x|we2 = x|addr1 = x|addr2 = x|data_in1 =   x|data_in2 =   x|data_out1 =   x|data_out2 =   x
+[SCOREBOARD ] |time = 10|clk = 1|rst = 0|we1  = 0|addr1 = 0|data_in1 = 0|data_out1 = 0|READ TEST PASSED
+[SCOREBOARD ] |time = 10|clk = 1|rst = 0|we2  = 0|addr2 = 0|data_in2 = 0|data_out2 = 0|READ TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 30 |clk = 1|rst = 1 |we1 = 1|we2 = 0|addr1 = 0|addr2 = 3|data_in1 = 173|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[DRIVER  ] |time = 30 |clk = 1|rst = 1 |we1 = 1|we2 = 0|addr1 = 0|addr2 = 3|data_in1 = 173|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[MONITOR  ] |time = 30 |clk = 1|rst = 0 |we1 = 1|we2 = 1|addr1 = 1|addr2 = 3|data_in1 =   7|data_in2 = 169|data_out1 =   x|data_out2 =   x
+[SCOREBOARD ] |time = 30|clk = 1|rst = 0|we1 = 1|addr1 = 1|data_in1 = 7|data_out1 = 0|WRITE TEST PASSED
+[SCOREBOARD ] |time = 30|clk = 1|rst = 0|we2 = 1|addr2 = 3|data_in2 = 169|data_out2 = 0|WRITE TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 50 |clk = 1|rst = 0 |we1 = 0|we2 = 0|addr1 = 0|addr2 = 3|data_in1 =   0|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[DRIVER  ] |time = 50 |clk = 1|rst = 0 |we1 = 0|we2 = 0|addr1 = 0|addr2 = 3|data_in1 =   0|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[MONITOR  ] |time = 50 |clk = 1|rst = 1 |we1 = 1|we2 = 0|addr1 = 0|addr2 = 3|data_in1 = 173|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[SCOREBOARD  ] |time = 50 |clk = 1|rst = 1 |we1 = 1|we2 = 0|addr1 = 0|addr2 = 3|data_in1 = 173|data_in2 =   0|data_out1 =   0|data_out2 =   0|RESET TEST PASSED
+_____________________________________________________________________
+[GENERATOR  ] |time = 70 |clk = 1|rst = 0 |we1 = 0|we2 = 1|addr1 = 1|addr2 = 4|data_in1 =   0|data_in2 = 162|data_out1 =   0|data_out2 =   0
+[DRIVER  ] |time = 70 |clk = 1|rst = 0 |we1 = 0|we2 = 1|addr1 = 1|addr2 = 4|data_in1 =   0|data_in2 = 162|data_out1 =   0|data_out2 =   0
+[MONITOR  ] |time = 70 |clk = 1|rst = 0 |we1 = 0|we2 = 0|addr1 = 0|addr2 = 3|data_in1 =   0|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[SCOREBOARD ] |time = 70|clk = 1|rst = 0|we1  = 0|addr1 = 0|data_in1 = 0|data_out1 = 0|READ TEST PASSED
+[SCOREBOARD ] |time = 70|clk = 1|rst = 0|we2  = 0|addr2 = 3|data_in2 = 0|data_out2 = 0|READ TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 90 |clk = 1|rst = 1 |we1 = 1|we2 = 0|addr1 = 1|addr2 = 4|data_in1 = 193|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[DRIVER  ] |time = 90 |clk = 1|rst = 1 |we1 = 1|we2 = 0|addr1 = 1|addr2 = 4|data_in1 = 193|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[MONITOR  ] |time = 90 |clk = 1|rst = 0 |we1 = 0|we2 = 1|addr1 = 1|addr2 = 4|data_in1 =   0|data_in2 = 162|data_out1 =   7|data_out2 =   0
+[SCOREBOARD ] |time = 90|clk = 1|rst = 0|we1  = 0|addr1 = 1|data_in1 = 0|data_out1 = 7|READ TEST PASSED
+[SCOREBOARD ] |time = 90|clk = 1|rst = 0|we2 = 1|addr2 = 4|data_in2 = 162|data_out2 = 0|WRITE TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 110 |clk = 1|rst = 0 |we1 = 0|we2 = 1|addr1 = 1|addr2 = 7|data_in1 =   0|data_in2 = 132|data_out1 =   0|data_out2 =   0
+[DRIVER  ] |time = 110 |clk = 1|rst = 0 |we1 = 0|we2 = 1|addr1 = 1|addr2 = 7|data_in1 =   0|data_in2 = 132|data_out1 =   0|data_out2 =   0
+[MONITOR  ] |time = 110 |clk = 1|rst = 1 |we1 = 1|we2 = 0|addr1 = 1|addr2 = 4|data_in1 = 193|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[SCOREBOARD  ] |time = 110 |clk = 1|rst = 1 |we1 = 1|we2 = 0|addr1 = 1|addr2 = 4|data_in1 = 193|data_in2 =   0|data_out1 =   0|data_out2 =   0|RESET TEST PASSED
+_____________________________________________________________________
+[GENERATOR  ] |time = 130 |clk = 1|rst = 0 |we1 = 1|we2 = 0|addr1 = 5|addr2 = 4|data_in1 =  72|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[DRIVER  ] |time = 130 |clk = 1|rst = 0 |we1 = 1|we2 = 0|addr1 = 5|addr2 = 4|data_in1 =  72|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[MONITOR  ] |time = 130 |clk = 1|rst = 0 |we1 = 0|we2 = 1|addr1 = 1|addr2 = 7|data_in1 =   0|data_in2 = 132|data_out1 =   0|data_out2 =   0
+[SCOREBOARD ] |time = 130|clk = 1|rst = 0|we1  = 0|addr1 = 1|data_in1 = 0|data_out1 = 0|READ TEST PASSED
+[SCOREBOARD ] |time = 130|clk = 1|rst = 0|we2 = 1|addr2 = 7|data_in2 = 132|data_out2 = 0|WRITE TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 150 |clk = 1|rst = 1 |we1 = 1|we2 = 0|addr1 = 3|addr2 = 4|data_in1 =  18|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[DRIVER  ] |time = 150 |clk = 1|rst = 1 |we1 = 1|we2 = 0|addr1 = 3|addr2 = 4|data_in1 =  18|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[MONITOR  ] |time = 150 |clk = 1|rst = 0 |we1 = 1|we2 = 0|addr1 = 5|addr2 = 4|data_in1 =  72|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[SCOREBOARD ] |time = 150|clk = 1|rst = 0|we1 = 1|addr1 = 5|data_in1 = 72|data_out1 = 0|WRITE TEST PASSED
+[SCOREBOARD ] |time = 150|clk = 1|rst = 0|we2  = 0|addr2 = 4|data_in2 = 0|data_out2 = 0|READ TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 170 |clk = 1|rst = 1 |we1 = 0|we2 = 0|addr1 = 0|addr2 = 4|data_in1 =   0|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[DRIVER  ] |time = 170 |clk = 1|rst = 1 |we1 = 0|we2 = 0|addr1 = 0|addr2 = 4|data_in1 =   0|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[MONITOR  ] |time = 170 |clk = 1|rst = 1 |we1 = 1|we2 = 0|addr1 = 3|addr2 = 4|data_in1 =  18|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[SCOREBOARD  ] |time = 170 |clk = 1|rst = 1 |we1 = 1|we2 = 0|addr1 = 3|addr2 = 4|data_in1 =  18|data_in2 =   0|data_out1 =   0|data_out2 =   0|RESET TEST PASSED
+_____________________________________________________________________
+[GENERATOR  ] |time = 190 |clk = 1|rst = 0 |we1 = 1|we2 = 1|addr1 = 1|addr2 = 2|data_in1 = 195|data_in2 = 126|data_out1 =   0|data_out2 =   0
+[DRIVER  ] |time = 190 |clk = 1|rst = 0 |we1 = 1|we2 = 1|addr1 = 1|addr2 = 2|data_in1 = 195|data_in2 = 126|data_out1 =   0|data_out2 =   0
+[MONITOR  ] |time = 190 |clk = 1|rst = 1 |we1 = 0|we2 = 0|addr1 = 0|addr2 = 4|data_in1 =   0|data_in2 =   0|data_out1 =   0|data_out2 =   0
+[SCOREBOARD  ] |time = 190 |clk = 1|rst = 1 |we1 = 0|we2 = 0|addr1 = 0|addr2 = 4|data_in1 =   0|data_in2 =   0|data_out1 =   0|data_out2 =   0|RESET TEST PASSED
+_____________________________________________________________________
+[GENERATOR  ] |time = 210 |clk = 1|rst = 0 |we1 = 0|we2 = 1|addr1 = 5|addr2 = 5|data_in1 =   0|data_in2 = 248|data_out1 =   0|data_out2 =   0
+[DRIVER  ] |time = 210 |clk = 1|rst = 0 |we1 = 0|we2 = 1|addr1 = 5|addr2 = 5|data_in1 =   0|data_in2 = 248|data_out1 =   0|data_out2 =   0
+[MONITOR  ] |time = 210 |clk = 1|rst = 0 |we1 = 1|we2 = 1|addr1 = 1|addr2 = 2|data_in1 = 195|data_in2 = 126|data_out1 =   0|data_out2 =   0
+[SCOREBOARD ] |time = 210|clk = 1|rst = 0|we1 = 1|addr1 = 1|data_in1 = 195|data_out1 = 0|WRITE TEST PASSED
+[SCOREBOARD ] |time = 210|clk = 1|rst = 0|we2 = 1|addr2 = 2|data_in2 = 126|data_out2 = 0|WRITE TEST PASSED
+________________________________________________________________
+[GENERATOR  ] |time = 230 |clk = 1|rst = 0 |we1 = 0|we2 = 1|addr1 = 5|addr2 = 5|data_in1 =   0|data_in2 = 199|data_out1 =   0|data_out2 =   0
+[DRIVER  ] |time = 230 |clk = 1|rst = 0 |we1 = 0|we2 = 1|addr1 = 5|addr2 = 5|data_in1 =   0|data_in2 = 199|data_out1 =   0|data_out2 =   0
+[MONITOR  ] |time = 230 |clk = 1|rst = 0 |we1 = 0|we2 = 1|addr1 = 5|addr2 = 5|data_in1 =   0|data_in2 = 248|data_out1 = 248|data_out2 =   0
+[SCOREBOARD ] |time =  230|clk = 1|rst = 0|we1 = 0|addr1 = 5 |data_in1 = 0 |data_out1 = 248 |READ TEST FAILED
+[SCOREBOARD ] |time = 230|clk = 1|rst = 0|we2 = 1|addr2 = 5|data_in2 = 248|data_out2 = 0|WRITE TEST PASSED
+________________________________________________________________
+$finish at simulation time                  230
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 230 ns
+CPU Time:      0.810 seconds;       Data structure size:   0.0Mb
+Fri May 29 03:29:56 2026
+Done
