7554cf684b86e1a5b5f9cbae458c5aaff28432a1
Manogaran17 <manogaran2667@gmail.com>
2026-05-29 13:11:28 +0530

    Update 04-TRANSACTION.sv

--- 04-MEMORY/02-DRAM/04-TRANSACTION.sv
+++ 04-MEMORY/02-DRAM/04-TRANSACTION.sv
@@ -1,1 +1,63 @@
+class transaction;
 
+  rand bit rst;
+
+  // ================= PORT-1 =================
+
+  rand bit        we1;
+  rand bit [2:0]  addr1;
+  rand bit [7:0]  data_in1;
+
+  bit [7:0] data_out1;
+
+  // ================= PORT-2 =================
+
+  rand bit        we2;
+  rand bit [2:0]  addr2;
+  rand bit [7:0]  data_in2;
+
+  bit [7:0] data_out2;
+
+  rand bit [1:0] op_mode;
+
+  /*
+      00 -> READ  READ
+      01 -> READ  WRITE
+      10 -> WRITE READ
+      11 -> WRITE WRITE
+  */
+
+  constraint operation_c
+  {
+      (op_mode==2'b00)->(we1==0&&we2==0);
+
+      (op_mode==2'b01)->(we1==0&&we2==1);
+
+      (op_mode==2'b10)->(we1==1&&we2==0);
+
+      (op_mode==2'b11)->(we1==1&&we2==1);
+
+  }
+
+
+  constraint rd_c
+  {
+      if(we1==0)
+          data_in1==0;
+
+      if(we2==0)
+          data_in2==0;
+  }
+
+
+  constraint mode_dist
+  {
+      op_mode dist
+      {
+          2'b00:=3,
+          2'b01:=3,
+          2'b10:=3,
+          2'b11:=3
+      };
+  }
+endclass
