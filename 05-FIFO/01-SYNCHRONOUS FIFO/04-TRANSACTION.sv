8608346628426e59fac7ed2c878fbffc1d255388
Manogaran17 <manogaran2667@gmail.com>
2026-05-30 18:52:32 +0530

    Update 04-TRANSACTION.sv

--- 05-FIFO/01-SYNCHRONOUS FIFO/04-TRANSACTION.sv
+++ 05-FIFO/01-SYNCHRONOUS FIFO/04-TRANSACTION.sv
@@ -1,1 +1,45 @@
+class transaction;
 
+  static int count;
+
+  rand bit rst;
+  rand bit we;
+  rand bit re;
+
+  rand bit [7:0] data_in;
+
+  bit [7:0] data_out;
+  bit full;
+  bit empty;
+  // Reset only for first transaction
+  constraint rst_c
+  {
+    if(count==0)
+      rst==1;
+    else
+      rst==0;
+  }
+  // 5 writes followed by 5 reads
+  constraint fifo_c
+  {
+    if(count>=0&&count<=15)
+    {
+      we==1;
+      re==0;
+    }
+      if(count>=16&&count<=26)
+    {
+      we==0;
+      re==1;
+    }
+  }
+  constraint read_c
+  {
+    if(re)
+      data_in==0;
+  }
+  function void post_randomize();
+    count++;
+  endfunction
+
+endclass
