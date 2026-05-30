728ca7b15f7d05b4faf2e0cd23354036a0891089
Manogaran17 <manogaran2667@gmail.com>
2026-05-30 18:53:21 +0530

    Update 06-DRIVER.sv

--- 05-FIFO/01-SYNCHRONOUS FIFO/06-DRIVER.sv
+++ 05-FIFO/01-SYNCHRONOUS FIFO/06-DRIVER.sv
@@ -1,1 +1,53 @@
 
+class driver;
+  
+  transaction trans;
+  
+  mailbox #(transaction) gentodrv;
+  
+  virtual inter vif;
+  
+  
+  function new(virtual inter vif, mailbox #(transaction) gentodrv);
+    
+    this.vif =  vif;
+    
+    this.gentodrv = gentodrv;
+    
+    endfunction
+  
+  task main();
+    
+    repeat(26)
+      begin
+        
+           @(vif.cb);
+        
+        gentodrv.get(trans);
+        
+        
+        vif.cb.rst <= trans.rst;
+        vif.cb.we <= trans.we;
+        vif.cb.re <= trans.re;
+        vif.cb.data_in <= trans.data_in;
+        
+           
+        $display("[DRIVER  ] |time = %0t |clk = %0b|rst = %b |we = %b|re = %b|full = %b|empty = %b|data_in = %d|data_out = %d",
+                $time,
+                 vif.clk,
+                 trans.rst,
+                 trans.we,
+                 trans.re,
+                 trans.full,
+                 trans.empty,
+                 trans.data_in,
+                 trans.data_out
+                );
+      end 
+    
+  endtask
+  
+endclass:driver 
+
+
+
