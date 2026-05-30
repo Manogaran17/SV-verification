bad11a0f0f9fc3f68ba01145b5b9563e7d990df0
Manogaran17 <manogaran2667@gmail.com>
2026-05-30 18:55:04 +0530

    Update 09-ENVIRONMENT.sv

--- 05-FIFO/01-SYNCHRONOUS FIFO/09-ENVIRONMENT.sv
+++ 05-FIFO/01-SYNCHRONOUS FIFO/09-ENVIRONMENT.sv
@@ -1,1 +1,44 @@
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
 
+class environment;
+
+    generator gen;
+    driver drv;
+    monitor mon;
+    scoreboard scb;
+  
+    mailbox #(transaction)gentodrv;
+    mailbox #(transaction)montoscb;
+
+    virtual inter vif;
+
+  function new(virtual inter vif);
+
+        this.vif = vif;
+
+        gentodrv = new();
+        montoscb = new();
+      
+        gen = new(vif,gentodrv);
+        drv = new(vif,gentodrv);
+        mon = new(montoscb,vif);
+        scb = new(vif,montoscb);
+
+    endfunction
+
+    task run();
+
+        fork
+            gen.main();
+            drv.main();
+            mon.main();
+            scb.main();
+        join_any
+
+    endtask
+
+endclass
