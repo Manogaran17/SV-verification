6c66a6903c1e72ece28b6492e0940f66d581479d
Manogaran17 <manogaran2667@gmail.com>
2026-05-30 18:50:26 +0530

    Update 03-INTERFACE.sv

--- 05-FIFO/01-SYNCHRONOUS FIFO/03-INTERFACE.sv
+++ 05-FIFO/01-SYNCHRONOUS FIFO/03-INTERFACE.sv
@@ -1,1 +1,29 @@
-
+interface inter ( input logic clk);
+  
+  logic rst;
+  logic we;
+  logic re;
+ 
+  logic [7:0]data_in;
+  
+  logic [7:0]data_out;
+  logic full;
+  logic empty;
+  
+  clocking cb @(posedge clk );
+    
+    default input #1step output #0;
+    
+    output  rst;
+    output  we;
+    output  re;
+   
+    output data_in;
+    
+    input  data_out;
+    input full;
+    input empty;
+    
+  endclocking 
+  
+endinterface 
